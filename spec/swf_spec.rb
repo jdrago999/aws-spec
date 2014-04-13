7238b461dfb51c20d377c1fa19cb5fd4f4e0975d
oko_chang <hideaki.yanase@gmail.com>
2014-04-13 18:56:08 +0900

    added swf_spec.rb

---
+++ spec/swf_spec.rb
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+require_relative "spec_helper"
+
+describe "SimpleWorkflow" do
+  swf = AWS::SimpleWorkflow.new
+
+  describe "Domains" do
+    subject(:domains) do
+      swf.domains
+    end
+    it { should have(0).domains }
+  end
+
+end
