511a8fb7f2e5fac3e7a67520a94e90da0ab614a6
oko_chang <hideaki.yanase@gmail.com>
2014-04-13 15:54:31 +0900

    added autoscaling_spec.rb

---
+++ spec/autoscaling_spec.rb
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+require_relative "spec_helper"
+
+describe "AutoScaling" do
+  auto_scaling = AWS::AutoScaling.new
+
+  describe "Auto Scaling Groups" do
+    subject(:auto_scaling_groups) do
+      auto_scaling.groups
+    end
+    it { should have(0).auto_scaling_groups }
+  end
+
+  describe "Launch Configurations" do
+    subject(:launch_configurations) do
+      auto_scaling.launch_configurations
+    end
+    it { should have(0).launch_configurations }
+  end
+
+  describe "Scheduled Actions" do
+    subject(:scheduled_actions) do
+      auto_scaling.scheduled_actions
+    end
+    it { should have(0).scheduled_actions }
+  end
+  
+end
