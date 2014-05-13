7dc1acbafb1ccdb160909f4c0a1cfdd3550537f7
Daichi Arai <daichi2k@gmail.com>
2014-05-13 15:06:24 +0900

    AWS.config direct load from file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,3 @@
 require 'aws-sdk'
 
-if File.exist?("config/aws.yml")
-  config = YAML.load_file("config/aws.yml")
-  AWS.config(
-    access_key_id: config["access_key_id"],
-    secret_access_key: config["secret_access_key"],
-    region: config["region"]
-  )
-end
+AWS.config(YAML.load_file("config/aws.yml")) if File.exist?("config/aws.yml")
